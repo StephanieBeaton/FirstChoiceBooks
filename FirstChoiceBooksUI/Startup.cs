ed08ed619c3945f7bfed96f89798d7101d0abd95
Stephanie Beaton <stefanybeaton@gmail.com>
2016-08-07 09:43:37 -0700

    added ASP MVC front end project named FirstChoiceBooksUI to the solution. 7/31/2016

---
+++ FirstChoiceBooksUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstChoiceBooksUI.Startup))]
+namespace FirstChoiceBooksUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
